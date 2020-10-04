f2c2a94ce0252836f1381f29d497f4d16887bc93
Alice <alice.capponi@softwareuno.it>
2020-10-04 11:26:41 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xe-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'xe-app-post-angular';
+}
